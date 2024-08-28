658bf0533c6e2018fb694edfb8b4d79476891b98
phillip <philipmoyom@gmail.com>
2024-08-28 17:07:39 +0200

    banner string accessed from string literals

--- src/components/sections/onboarding/BannerComponent.tsx
+++ src/components/sections/onboarding/BannerComponent.tsx
@@ -3,8 +3,8 @@ import { onboardingScreenStyles } from "../../../styles/screensStyle/publicStyle
 import TextInputComponent from "../../common/Form/TextInputComponent";
 import { useState } from "react";
 import GradientButtonComponent from "../../common/Form/GradientButtonComponent";
-import AuthTextField from "../../common/Form/AuthTextField";
 import { IMAGES } from "../../../constants";
+import { STRING } from "../../../constants/strings";
 
 const OnboardingTopImageComponent = () => {
   const [search, setSearch] = useState<string>("");
@@ -17,17 +17,13 @@ const OnboardingTopImageComponent = () => {
     >
       <View style={onboardingScreenStyles.imageTextContainer}>
         <Text style={onboardingScreenStyles.imageTitleItem}>
-          Personalised Learning, Anytime, Anywhere
+          {STRING.bannerTopic}
         </Text>
         <Text style={onboardingScreenStyles.imageTextInfoItem}>
-          Welcome to Acastudy, where education is tailored to fit your needs!
-          Our platform connects you with expert tutors who are ready to help you
-          achieve academic success. Whether it's a quick session or a scheduled
-          deep dive, Acastudy provides flexible, on-demand learning experiences
-          that adapt to your goals and availability.
+          {STRING.bannerContent}
         </Text>
         <Text style={onboardingScreenStyles.imageTextInfoItem}>
-          Take control of your academic journey and thrive with Acastudy today!
+          {STRING.bannerSubContent}
         </Text>
       </View>
 
@@ -44,11 +40,7 @@ const OnboardingTopImageComponent = () => {
         </View>
 
         <View style={onboardingScreenStyles.searchButtonContainer}>
-          <GradientButtonComponent
-            text="Search"
-            onPress={() => {
-              console.log("search pressed");
-            }}
+          <GradientButtonComponent text="Search" onPress={() => { console.log("search pressed") }}
           />
         </View>
       </View>
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -10,6 +10,13 @@ export const STRING = {
     requestTutor: 'Request tutor',
     signIn: 'SignIn',
     signUp: 'SignUp',
+    bannerTopic: 'Personalised Learning, Anytime, Anywhere',
+    bannerContent: `Welcome to Acastudy, where education is tailored to fit your needs!
+          Our platform connects you with expert tutors who are ready to help you
+          achieve academic success. Whether it's a quick session or a scheduled
+          deep dive, Acastudy provides flexible, on-demand learning experiences
+          that adapt to your goals and availability.`,
+    bannerSubContent: 'Take control of your academic journey and thrive with Acastudy today!',
 };
 
 export const NAV_SCREEN_NAME = {
