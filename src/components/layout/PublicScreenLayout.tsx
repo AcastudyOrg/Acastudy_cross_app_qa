fe014b2da7ad0d0db476f6631e5d157bdaf996b2
phillip <philipmoyom@gmail.com>
2024-08-28 14:37:28 +0200

    removed the extra topbar and use already existing

--- src/components/layout/PublicScreenLayout.tsx
+++ src/components/layout/PublicScreenLayout.tsx
@@ -4,37 +4,59 @@ import { View, SafeAreaView, ScrollView } from "react-native";
 import { screenSize } from "../../../utils/config";
 import { DEVICE_TYPE } from "../../constants/strings";
 import { privateScreenLayoutStyles } from "../../styles/componentsStyle/layoutStyle/privateScreenLayoutStyle";
-import OnboardingTopBar from "../common/TopBar/OnboardingTopBar";
+import TopBarComponent from "../common/TopBar/TopBarComponent";
+import { User } from "../../types/User/Student";
 
 const PublicScreenLayout = ({ children }: { children: React.ReactNode }) => {
-  const size = screenSize();
-  return (
-    <SafeAreaView style={privateScreenLayoutStyles.layoutContainer}>
-      {size === DEVICE_TYPE.desktop ||
-      (DEVICE_TYPE.tablet && size !== DEVICE_TYPE.mobile) ? (
-        <View style={privateScreenLayoutStyles.topNavContainer}>
-          <OnboardingTopBar />
-          <ScrollView
-            style={privateScreenLayoutStyles.childrenScrollView}
-            showsVerticalScrollIndicator={false}
-          >
-            <View style={privateScreenLayoutStyles.mainContent}>
-              {children}
-            </View>
-          </ScrollView>
-        </View>
-      ) : (
-        <View style={privateScreenLayoutStyles.mobileScrollViewContainer}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <OnboardingTopBar />
-            <View style={privateScreenLayoutStyles.mainContent}>
-              {children}
-            </View>
-          </ScrollView>
-        </View>
-      )}
-    </SafeAreaView>
-  );
+    const size = screenSize();
+
+    const user: User = {
+        name: "",
+        surname: "",
+        profilePictureUrl: 0
+    };
+
+    return (
+        <SafeAreaView style={privateScreenLayoutStyles.layoutContainer}>
+            {size === DEVICE_TYPE.desktop ||
+                (DEVICE_TYPE.tablet && size !== DEVICE_TYPE.mobile) ? (
+                <View style={privateScreenLayoutStyles.topNavContainer}>
+                    <TopBarComponent
+                        renderRightSection={true}
+                        showAppName={true}
+                        showSearchBar={false}
+                        isLSignedIn={false}
+                        user={user}
+                        showBecomeATutorOnly={false}
+                    />
+                    <ScrollView
+                        style={privateScreenLayoutStyles.childrenScrollView}
+                        showsVerticalScrollIndicator={false}
+                    >
+                        <View style={privateScreenLayoutStyles.mainContent}>
+                            {children}
+                        </View>
+                    </ScrollView>
+                </View>
+            ) : (
+                <View style={privateScreenLayoutStyles.mobileScrollViewContainer}>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        <TopBarComponent
+                            renderRightSection={true}
+                            showAppName={true}
+                            showSearchBar={false}
+                            isLSignedIn={false}
+                            user={user}
+                            showBecomeATutorOnly={false}
+                        />
+                        <View style={privateScreenLayoutStyles.mainContent}>
+                            {children}
+                        </View>
+                    </ScrollView>
+                </View>
+            )}
+        </SafeAreaView>
+    );
 };
 
 export default PublicScreenLayout;
