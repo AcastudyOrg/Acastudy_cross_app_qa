9100e90b69200607eb116d863ee1dcb19926ec05
phillip <philipmoyom@gmail.com>
2024-09-15 13:12:29 +0200

    -tutors top bar

---
+++ src/styles/componentsStyle/sectionsStyle/tutorsProfile/tutorsHeader.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../../../constants';
+
+export const tutorHeaderStyles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingTop: 10,
+
+    },
+    header: {
+        padding: 20,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: "center",
+
+    },
+    profileImage: {
+        width: 150,
+        height: 150,
+        borderRadius: 50,
+    },
+    tutorHeaderInfo: {
+        paddingLeft: 20,
+        justifyContent: 'flex-start',
+    },
+    name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: COLORS.white,
+        marginTop: 10,
+    },
+    rating: {
+        fontSize: 16,
+        color: COLORS.white50Percent,
+    },
+    requestButton: {
+        justifyContent: 'flex-start',
+        paddingRight: 20,
+    }
+});
