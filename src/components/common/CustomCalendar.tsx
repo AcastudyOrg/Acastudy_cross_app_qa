e81455041d0afdba970e8a8334a615e7ebabc9a0
phillip <philipmoyom@gmail.com>
2024-09-22 19:03:30 +0200

    customCalendar

---
+++ src/components/common/CustomCalendar.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Calendar, DateData } from 'react-native-calendars';
+import { COLORS } from '../../constants';
+import fontFamily from '../../constants/fontFamily';
+
+interface CustomCalendarProps {
+	selectedDates: string[]; // Array of selected dates in 'YYYY-MM-DD' format e.g 2024-05-01
+}
+
+const CustomCalendar: React.FC<CustomCalendarProps> = ({ selectedDates }) => {
+
+	const markedDates = selectedDates.reduce((acc, date, index, array) => {
+		const prevDate = array[index - 1];
+		const nextDate = array[index + 1];
+
+		const currentDate = new Date(date).getTime();
+		const prevDateTime = prevDate ? new Date(prevDate).getTime() : null;
+		const nextDateTime = nextDate ? new Date(nextDate).getTime() : null;
+
+		const oneDay = 24 * 60 * 60 * 1000;
+
+		const isStandalone =
+			(!prevDateTime || currentDate - prevDateTime !== oneDay) &&
+			(!nextDateTime || nextDateTime - currentDate !== oneDay);
+
+		const isStartingDay = !prevDateTime || currentDate - prevDateTime !== oneDay;
+		const isEndingDay = !nextDateTime || nextDateTime - currentDate !== oneDay;
+
+		acc[date] = {
+			color: COLORS.darkGray,
+			textColor: COLORS.white,
+			startingDay: isStartingDay,
+			endingDay: isEndingDay,
+		};
+		return acc;
+	}, {} as { [key: string]: any });
+
+	return (
+		<Calendar
+			onDayPress={(day: DateData) => { console.log('selected day', day); }}
+			style={{ height: 350, width: 350 }}
+			markedDates={markedDates}
+			markingType={'period'}
+			theme={{
+				calendarBackground: COLORS.transparent,
+				textSectionTitleColor: COLORS.white,
+				selectedDayBackgroundColor: COLORS.green,
+				selectedDayTextColor: COLORS.white,
+				todayTextColor: COLORS.green,
+				dayTextColor: COLORS.white,
+				textDisabledColor: COLORS.darkGrayOpacity,
+				monthTextColor: COLORS.white,
+				textMonthFontWeight: 'bold',
+				textMonthFontFamily: fontFamily.plusJakartaExtraBold,
+			}}
+		/>
+	);
+}
+
+export default CustomCalendar;
