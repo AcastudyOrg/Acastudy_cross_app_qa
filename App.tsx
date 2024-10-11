a476d7e118b2555b1d4db7cbf2c54586b72c73dd
fankholor-sudo <vincentsebothoma945@gmail.com>
2024-10-11 08:32:45 +0200

    Deep linking hook

--- App.tsx
+++ App.tsx
@@ -3,18 +3,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
-import * as Linking from 'expo-linking';
 
 import { COLORS } from "./src/constants";
 import useFonts from "./src/hooks/useFonts";
 import AppMainNavigation from "./src/navigation";
 import { LoadingComponent } from "./src/components/";
+import useDeepLinking from "./src/hooks/useDeepLinking";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
   const fontsLoaded = useFonts();
+  const linking = useDeepLinking();
 
   useEffect(() => {
     async function prepare() {
@@ -31,61 +32,6 @@ export default function App() {
     prepare();
   }, [fontsLoaded]);
 
-  // ___________________Deep_Linking: Handle incoming links_______________________
-  const prefix = Linking.createURL("http://localhost:8081/");
-  React.useEffect(() => {
-    const handleDeepLink = (event: any) => {
-      const data = Linking.parse(event.url);
-      console.log("deeplink data: ", data);
-    };
-
-    const subscription = Linking.addEventListener("url", handleDeepLink);
-    Linking.getInitialURL().then((url) => {
-      if (url) {
-        handleDeepLink({ url });
-      }
-    });
-
-    return () => {
-      subscription.remove();
-    };
-  }, []);
-
-  const linking = {
-    prefixes: [prefix, "acastudy_ts_app"],
-    config: {
-      screens: {
-        OnboardingScreen: {
-          path: "web/onboarding",
-        },
-        PrivacyPolicyScreen: {
-          path: "web/privacypolicy",
-        },
-        TermsOfUseScreen: {
-          path: "web/termsofuse",
-        },
-        HomeScreen: {
-          path: "web/home",
-        },
-        CallScreen: {
-          path: "web/meeting-room",
-        },
-        ChatScreen: {
-          path: "web/chat",
-        },
-        StudentProfileScreen: {
-          path: "web/student-profile",
-        },
-        StudyScreen: {
-          path: "web/study",
-        },
-        TutorProfileScreen: {
-          path: "web/tutor-profile",
-        },
-      },
-    },
-  };
-  // ____________________Linking-End_____________________
 
   if (!appIsReady) {
     return <LoadingComponent />;
