d581caf109ade5461887ac044e12d9b95cbb35d5
Phutiane Chipu <phutiane.chipu@entelect.co.za>
2024-10-15 00:33:59 +0200

    Added new mock data

--- src/screens/private/RequestTutorScreen.tsx
+++ src/screens/private/RequestTutorScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Modal, View, Text, Button, StyleSheet, ScrollView,  } from 'react-native';
+import { View, Text, StyleSheet, ScrollView,  } from 'react-native';
 import filterLevelOfStudy from '../../../assets/data/subjects/filterLeveOfStudy.json';
 import filterTutorData from '../../../assets/data/subjects/filterTutorData.json';
 import { DropDownComponent } from '../../components/common/Form/DropDownComponent';
 import { tutorProfileStyles } from '../../styles/componentsStyle/commonStyle/tutorProfileStyle';
 import CustomCalendar from '../../components/common/CustomCalendar';
 import { tutorData } from '../../../mockData/TutorData';
+import CourseData from '../../../assets/data/subjects/CourseData.json'
 import CustomTextAreaInput from '../../components/common/Form/CustomTextAreaInput';
 import GradientButtonComponent from '../../components/common/Form/GradientButtonComponent';
 import PrivateScreenLayout from '../../components/layout/PrivateScreenLayout';
@@ -16,6 +17,60 @@ const RequestTutorScreen = () => {
     const [levelOfStudy, setLevelOfStudy] = useState("");
     const [tutor, setTutor] = useState("");
 
+    const [selectedCourse, setSelectedCourse] = useState<number | null>(null);
+    const [selectedTopic, setSelectedTopic] = useState<number | null>(null);
+    const [selectedTutor, setSelectedTutor] = useState<number | null>(null);
+    const [selectedBook, setSelectedBook] = useState<number | null>(null);
+    const [selectedBookChapter, setSelectedBookChapter] = useState<number | null>(null);
+
+
+    const coursesDropdownData = CourseData.courses.map((course) => ({
+      label: course.title,
+      value: course.id,
+      topics: course.topics, 
+      tutors: course.tutors,
+      books: course.books
+    }));
+  
+    const selectedCourseData = CourseData.courses.find(
+      (course) => course.id === selectedCourse
+    );
+
+    const topicsDropdownData = selectedCourseData
+      ? selectedCourseData.topics.map((topic) => ({
+          label: topic.name,
+          value: topic.id
+        }))
+
+      : [];
+  
+    const tutorsDropdownData = selectedCourseData
+      ? selectedCourseData.tutors.map((tutor, index) => ({
+          label: tutor,
+          value: index
+        }))
+      : [];
+  
+    const booksDropdownData = selectedCourseData
+      ? selectedCourseData.books.map((book, index) => ({
+          label: book.title,
+          chapters: book.chapters,
+          id: book.id
+        }))
+      : [];
+
+      const selectedBookData = booksDropdownData.find(
+        (book) => book.id === selectedBook
+      );
+console.log(selectedBook)
+    const chaptersDropdownData = selectedBookData
+  ? selectedBookData.chapters.map((chapter) => ({
+      label: chapter.title,
+      value: chapter.id, 
+    }))
+  : [];
+
+
   return (
     
 <PrivateScreenLayout showBackButton={true} showSearchBar={false}>
@@ -23,14 +78,20 @@ const RequestTutorScreen = () => {
         <ScrollView>
             <View style={styles.dropDownContainer}>
                 <View style={styles.Dropdown}>
-                    <DropDownComponent data={filterLevelOfStudy} label="Study level" placeholder="Level of study" value={levelOfStudy} onChangeText={setLevelOfStudy} />
-                    <DropDownComponent data={filterTutorData} label= "Topic" placeholder="Tutor" value={tutor} onChangeText={setTutor} />
-                    <DropDownComponent data={filterLevelOfStudy} label="Book" placeholder="Level of study" value={levelOfStudy} onChangeText={setLevelOfStudy} />
+                    <DropDownComponent data={filterLevelOfStudy} label="Study level" placeholder="Level of study" value={levelOfStudy} onChange={setLevelOfStudy} />
+                    <DropDownComponent data={topicsDropdownData} label= "Topic" placeholder="Pick a Topic" value={selectedTopic} onChange={setSelectedTopic} disabled={!selectedCourse}/>
+                    <DropDownComponent data={booksDropdownData} label="Book" placeholder="Choose preferred book" value={selectedBook} onChange={(value) => {
+                          setSelectedBook(value.id);
+                        }} disabled={!selectedCourse}/>
                 </View>
                 <View style={styles.Dropdown}>
-                    <DropDownComponent data={filterTutorData} label="Course" placeholder="Tutor" value={tutor} onChangeText={setTutor} />
-                    <DropDownComponent data={filterTutorData} label="Tutor" placeholder="Tutor" value={tutor} onChangeText={setTutor} />
-                    <DropDownComponent data={filterLevelOfStudy} label="Chapter"  placeholder="Level of study" value={levelOfStudy} onChangeText={setLevelOfStudy} />
+                    <DropDownComponent data={coursesDropdownData} label="Select Course" placeholder="Choose Course" value={selectedCourse} onChange={(value) => {
+                          setSelectedCourse(value.value);
+                          setSelectedTopic(null);
+                          setSelectedTutor(null);
+                        }} />
+                    <DropDownComponent data={tutorsDropdownData} label="Tutor" placeholder="Tutor" value={selectedTutor} onChange={setSelectedTutor} disabled={!selectedCourse} />
+                    <DropDownComponent data={chaptersDropdownData} label="Chapter"  placeholder="Select a chapter" value={selectedBookChapter} onChange={setSelectedBookChapter}  disabled={!selectedBook}/>
                 </View>
             </View>
             <View style={styles.input}>
@@ -46,7 +107,7 @@ const RequestTutorScreen = () => {
               </View>
           </View>
           <View style={styles.input}>
-            <DropDownComponent data={filterTutorData} label="Time" placeholder="Tutor" value={tutor} onChangeText={setTutor} />
+            <DropDownComponent data={filterTutorData} label="Time" placeholder="Tutor" value={tutor} onChange={setTutor} />
           </View>
 
             <View>
