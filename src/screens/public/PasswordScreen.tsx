5843373d4ac4d0b678a6e302f8d70f40107ac5c0
fankholor-sudo <vincentsebothoma945@gmail.com>
2025-03-16 07:05:10 +0200

    Forgot password Integration

--- src/screens/public/PasswordScreen.tsx
+++ src/screens/public/PasswordScreen.tsx
@@ -4,6 +4,7 @@ import {
 	View,
 } from "react-native";
 import { useNavigation, useRoute, RouteProp } from "@react-navigation/native";
+import { useMutation } from "@apollo/client";
 
 import { User } from "@/types/User/Student";
 import { GradientButtonComponent } from "@/components/";
@@ -11,17 +12,22 @@ import { getPasswordRules, validatePassword } from "utils/login";
 import TopBarComponent from "@/components/common/TopBar/TopBarComponent";
 import { authScreenStyle } from "@/styles/screensStyle/publicStyle/authScreenStyle";
 import AuthTextField from "@/components/common/Form/AuthTextField";
+import { changePasswordMutation } from "@/graphql/api/auth";
 import { NAV_SCREEN_NAME } from "@/constants/strings";
 
 type PasswordParams = {
 	data: {
 		email: string;
+		forgotPassword?: boolean;
 	};
 };
 
 const PasswordScreen = () => {
+	const navigation = useNavigation<any>();
 	const route = useRoute<RouteProp<PasswordParams, 'data'>>();
-	const { email } = route.params;
+	const { email, forgotPassword = false } = route.params;
+
+	const [changePassword, { loading }] = useMutation(changePasswordMutation);
 
 	const title: string = "Create password";
 	const subtitle: string = "Your password should be at least 8 characters long!";
@@ -32,14 +38,14 @@ const PasswordScreen = () => {
 		profilePictureUrl: 0
 	};
 
-	const navigation = useNavigation<any>();
-
 	const [password, setPassword] = useState<string>("");
 	const [confirmPassword, setConfirmPassword] = useState<string>("");
 	const [passwordError, setPasswordError] = useState<string>("")
 	const [passwordNotMatchingError, setPasswordNotMatching] = useState<string>("")
+	const [error, setError] = useState<string>("");
 
-	const handleSubmit = () => {
+	const handleSubmit = async () => {
+		setError("")
 		setPasswordError("")
 		setPasswordNotMatching("")
 		if (!validatePassword(password.trim())) {
@@ -50,7 +56,17 @@ const PasswordScreen = () => {
 			setPasswordNotMatching("Passwords do not match")
 			return;
 		}
-		navigation.navigate(NAV_SCREEN_NAME.QuestioneirScreen, { email, password });
+
+		if (forgotPassword) {
+			await changePassword({ variables: { email, password } }).then((res) => {
+				if (res.data.changePassword.status === 200)
+					navigation.navigate(NAV_SCREEN_NAME.SignInScreen);
+				else throw res.data.changePassword;
+			}).catch((err) => {
+				setError(err.message);
+			});
+		}
+		else navigation.navigate(NAV_SCREEN_NAME.QuestioneirScreen, { email, password });
 	};
 
 	return (
@@ -63,13 +79,13 @@ const PasswordScreen = () => {
 					<Text style={authScreenStyle.subtitle}>{subtitle}</Text>
 
 					<AuthTextField label={"Password"} value={password} onChangeText={setPassword} isPassword={true} error={passwordError} />
-					<AuthTextField label={"Confirm Password"} value={confirmPassword} onChangeText={setConfirmPassword} isPassword={true} />
+					<AuthTextField label={"Confirm Password"} value={confirmPassword} onChangeText={setConfirmPassword} isPassword={true} error={error} />
 
 					<View style={[authScreenStyle.alternative, { paddingTop: 0, paddingBottom: 10 }]}>
 						{passwordNotMatchingError ? <Text style={authScreenStyle.errorText}>{passwordNotMatchingError}</Text> : null}
 					</View>
 
-					<GradientButtonComponent text="CONTINUE" onPress={handleSubmit} />
+					<GradientButtonComponent text="CONTINUE" loading={loading} onPress={handleSubmit} />
 				</View>
 			</View>
 		</View>
--- src/screens/public/VerifyEmailScreen.tsx
+++ src/screens/public/VerifyEmailScreen.tsx
@@ -13,12 +13,13 @@ import { verifyOTPCodeMutation, sendVerificationCodeMutation } from "@/graphql/a
 type VerifyEmailParams = {
   VerifyEmail: {
     email: string;
-  };
+    forgotPassword?: boolean;
+  }
 };
 
 const VerifyEmailScreen = () => {
   const route = useRoute<RouteProp<VerifyEmailParams, 'VerifyEmail'>>();
-  const { email } = route.params;
+  const { email, forgotPassword = false } = route.params;
 
   const title: string = "Confirm email address";
   const subtitle: string = `Please enter the verification code sent to ${email} to complete your signup process.`;
@@ -52,7 +53,7 @@ const VerifyEmailScreen = () => {
 
     await verifyOTPCode({ variables: { email, verificationCode } }).then((res) => {
       if (res.data.verifyOTPCode.status === 200)
-        navigation.navigate(NAV_SCREEN_NAME.PasswordScreen, { email });
+        navigation.navigate(NAV_SCREEN_NAME.PasswordScreen, { email, forgotPassword });
       else throw res.data.verifyOTPCode;
     }).catch((err) => {
       setError(err.message);
