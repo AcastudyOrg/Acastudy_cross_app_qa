5b8176227fb4c16e5d548150d24e619c962c36c2
phillip <philipmoyom@gmail.com>
2025-05-08 15:17:19 +0200

    add TutorProfileEditebleScreen component with biography input and availability calendar

---
+++ src/screens/private/Tutor/TutorProfileEditebleScreen.tsx
@@ -0,0 +1,61 @@
+
+import React from "react";
+import { Text, View } from "react-native";
+
+import TutorBio from "../../../components/sections/tutorProfile/TutorBio";
+import TutoSubjectOfInterest from "../../../components/sections/tutorProfile/TutorSubjectOfInterest";
+import TutorExperience from "../../../components/sections/tutorProfile/TutorsExperience";
+import EventsSection from "../../../components/sections/home/EventsSection";
+import TutorReviews from "../../../components/sections/tutorProfile/TutorReview";
+import PrivateScreenLayout from "../../../components/layout/PrivateScreenLayout";
+import TutorHeader from "../../../components/sections/tutorProfile/TutorHeader";
+import { tutorData } from "../../../../mockData/TutorData";
+import { tutorProfileStyles } from "../../../styles/componentsStyle/commonStyle/tutorProfileStyle";
+import { AvailablilityCalender } from "../../../components/common/AvailablilityCalender";
+import CustomNoStrokeTextInput from "@/components/common/Form/CustomNoStrokeTextInput";
+
+interface TutorProfileScreenProps {
+
+}
+const TutorProfileEditebleScreen: React.FC<TutorProfileScreenProps> = ({ }) => {
+
+    const [biography, setBiography] = React.useState<string>("");
+
+    return (
+        <PrivateScreenLayout showBackButton={true} showSearchBar={false}>
+            <View style={tutorProfileStyles.tutorProfileContainer}>
+                <TutorHeader
+                    name={tutorData.name}
+                    rating={tutorData.rating}
+                    reviews={tutorData.reviews}
+                    imageUrl={tutorData.imageUrl}
+                />
+                <TutorBio text={tutorData.bio} />
+                <CustomNoStrokeTextInput
+                    value={biography}
+                    label={"Bio"}
+                    placeholder={tutorData?.bio}
+                    multiline={true}
+                    onChange={setBiography} />
+
+                <TutoSubjectOfInterest subjects={tutorData.subjects} />
+                <TutorExperience experiences={tutorData.experiences} />
+                <EventsSection upcomingEventsData={tutorData.upcomingEvents} showViewMoreButton={tutorData.upcomingEvents.length > 4} />
+                <TutorReviews rating={tutorData.rating} reviewCounts={tutorData.reviewCounts} />
+
+                <View style={tutorProfileStyles.availabilitySection}>
+                    <Text style={tutorProfileStyles.availability}>Availability</Text>
+                    <View>
+                        <AvailablilityCalender
+                            onDateSelect={() => { }}
+                            selectedTutor={tutorData.bookedOutDates || undefined} // pass in the tutor you have selected
+                            minDate={new Date().toISOString().split('T')[0]}
+                        />
+                    </View>
+                </View>
+            </View>
+        </PrivateScreenLayout >
+    );
+}
+
+export default TutorProfileEditebleScreen;
